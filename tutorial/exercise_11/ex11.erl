54c1ba146b6099444f7f4591741bb9c50d8ff4de
lvonw <91526397+lvonw@users.noreply.github.com>
2024-01-15 16:43:58 +0100

    Update ex11.erl

--- tutorial/exercise_11/ex11.erl
+++ tutorial/exercise_11/ex11.erl
@@ -9,3 +9,50 @@ rpc(Pid, Request) ->
        after 250 -> 
            unreachable
     end. 
+
+% We have informed all processes that we are Coordinator
+% Continute business as usual
+informOfCoordinator([], AllPs) ->
+    process(AllPs, AllPs);
+
+informOfCoordinator([OtherP | OtherPs], AllPs) ->
+    OtherP ! {inform, self()},
+    informOfCoordinator(OtherPs, AllPs).
+
+% Start Process
+process(AllPs) -> 
+    process(AllPs, AllPs).
+
+% We are coordinator now, so we must inform all processes now
+process([], AllPs) ->
+    informOfCoordinator(AllPs, AllPs);
+
+process([OtherP | OtherPs], AllPs) ->
+    receive 
+        {Pid, election} ->
+            Pid ! {self(), ok},
+            process(AllPs, AllPs); 
+        {startElection} ->
+            if 
+                (OtherP > self()) ->
+                    Resp = rpc(OtherP, election),
+                    case Resp of
+                        ok ->
+                                OtherP ! startElection,
+                                process(AllPs, AllPs); 
+                        unreachable ->
+                            process(OtherPs, AllPs)
+                    end;
+                true ->
+                    process(OtherPs, AllPs)
+            end;
+        {inform, Pid} ->
+            ok
+    end.    
+
+% Assignment b
+
+setup() ->
+    
+                
+
